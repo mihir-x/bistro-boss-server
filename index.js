e8aaa9a10183193d036ca07c5b15c007968db78d
mihir-x <ictlancerx@gmail.com>
2023-11-16 00:44:42 +0600

    added user related api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -29,7 +29,46 @@ async function run() {
         const menuCollection = client.db('bistroDB').collection('menu')
         const reviewsCollection = client.db('bistroDB').collection('reviews')
         const cartsCollection = client.db('bistroDB').collection('carts')
+        const userCollection = client.db('bistroDB').collection('users')
 
+        //user related api
+        app.get('/users', async(req, res)=>{
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+        app.post('/users', async(req, res)=>{
+            const user = req.body
+            //insert email if user doesn't exists
+            //different ways to do this(1.make email unique 2.simple checking 3.upsert true)
+            const query = {email: user.email}
+            const existingUser = await userCollection.findOne(query)
+            if(existingUser){
+                return res.send({message: 'user already exists in database', insertedId: null})
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        app.patch('/users/admin/:id', async(req, res) =>{
+            const id = req.params
+            const query = { _id: new ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    role: 'Admin'
+                }
+            }
+            const result = await userCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        //menu related api
         app.get('/menu', async(req, res)=>{
             const result = await menuCollection.find().toArray()
             res.send(result)
@@ -49,6 +88,14 @@ async function run() {
         app.post('/carts', async(req, res) =>{
             const cart = req.body
             const result = await cartsCollection.insertOne(cart)
+            res.send(result)
+        })
+        app.delete('/carts/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await cartsCollection.deleteOne(query)
             res.send(result)
         })
 
