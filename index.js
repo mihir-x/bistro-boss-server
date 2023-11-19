cf9f9f4c66909d5eaf7778af46b59636f222b472
mihir-x <ictlancerx@gmail.com>
2023-11-19 20:51:49 +0600

    added payment intent

--- index.js
+++ index.js
@@ -188,6 +188,23 @@ async function run() {
         })
 
 
+        //payment intent
+        app.post('/create-payment-intent', async(req, res) =>{
+            const {price} = req.body
+            const amount = parseInt(price*100)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: [
+                    'card'
+                ]
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
